c0e568b6312d388d7bd2677cd504c1c7e31f1994
Lucas Machado <lucasml.bh@gmail.com>
2022-08-08 20:54:12 -0300

    Project creation and page navigation

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatelessWidget {
+  final _pageController = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Container(color: Colors.yellow),
+        Container(color: Colors.red),
+        Container(color: Colors.green),
+      ],
+    );
+  }
+}
