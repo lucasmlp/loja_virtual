a40a3110a329b638c89bf5c598109f41ae4bf121
Lucas Machado <lucasml.bh@gmail.com>
2022-08-22 20:22:06 -0300

    Adding app bar to home tab

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -9,12 +9,29 @@ class HomeTab extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget _buildBodyBack() => Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 211, 118, 130),
-              Color.fromARGB(255, 253, 181, 168),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 211, 118, 130),
+            Color.fromARGB(255, 253, 181, 168),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         );
-    return _buildBodyBack();
+    return Stack(
+      children: [
+        _buildBodyBack(),
+        const CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('Novidades'),
+                centerTitle: true,
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
